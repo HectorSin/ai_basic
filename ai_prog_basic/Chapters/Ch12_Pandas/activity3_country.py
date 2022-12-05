e3f29ce9bb7616142824d36cbf62360451591969
Jaeyun Sin <kkang15634@ajou.ac.kr>
2022-12-05 14:38:12 +0900

    blocks

--- ai_prog_basic/Chapters/Ch12_Pandas/activity3_country.py
+++ ai_prog_basic/Chapters/Ch12_Pandas/activity3_country.py
@@ -1,8 +1,17 @@
+# In[1]:
 import pandas as pd
 import matplotlib.pyplot as plt
 
 url = "https://raw.githubusercontent.com/HectorSin/ai_basic/master/ai_prog_basic/Chapters/Ch12_Pandas/data/countries.csv"
 country = pd.read_csv(url, index_col = 0)
 
+# In[2]:
 country['population'].plot(kind='bar', color=('b', 'darkorange', 'g', 'r', 'm'))
 plt.show()
+
+# In[3]:
+country['density'] = country['population'] / country['area']
+print(country)
+
+# In[4]:
+
